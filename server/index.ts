9556360f7e52a1078d6e128472205d0893efc883
sabir12hj <sk8741093086@gmail.com>
2025-05-11 13:53:14 -0700

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, { type Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import cors from "cors";
